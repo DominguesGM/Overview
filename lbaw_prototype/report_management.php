1d82d5f90b706b4a5b6b2650b569a5f26c745535
Gil Domingues <gmdz95@gmail.com>
2016-03-16 23:52:01 +0000

    Report management page done

---
+++ lbaw_prototype/report_management.php
@@ -0,0 +1,79 @@
+<?php
+include('template/header.html');
+?>
+
+<!-- search CSS -->
+<link href="css/report_management.css" rel="stylesheet">
+
+<div class="container">
+    <div class="col-md-12">
+        <hgroup class="mb20">
+            <br>
+            <h1>Reported Posts</h1>
+            <h2 class="lead"><strong>3</strong> Posts have been reported and need your attention</h2>
+        </hgroup>
+    </div>
+
+    <div class="col-md-12">
+        <section class="col-md-12 col-sm-6 col-md-18">
+            <article class="search-result row">
+                <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
+                <div class="col-xs-12 col-sm-12 col-md-2">
+                    <ul class="meta-search">
+                        <li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+                        <li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+                        <li><i class="glyphicon glyphicon-tags"></i> <span>Category1</span></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+                    <h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+                    <span class="tick"><a href="#" title="Item reviewed"><i class="fa fa-check"></i></a></span>
+                    <span class="cross"><a href="#" title="Remove article"><i class="fa fa-times"></i></a></span>
+                </div>
+                <span class="clearfix borda"></span>
+            </article>
+
+            <article class="search-result row">
+                <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
+                <div class="col-xs-12 col-sm-12 col-md-2">
+                    <ul class="meta-search">
+                        <li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+                        <li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+                        <li><i class="glyphicon glyphicon-tags"></i> <span>Category1</span></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+                    <h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+                    <span class="tick"><a href="#" title="Item reviewed"><i class="fa fa-check"></i></a></span>
+                    <span class="cross"><a href="#" title="Remove article"><i class="fa fa-times"></i></a></span>
+                </div>
+                <span class="clearfix borda"></span>
+            </article>
+
+
+            <article class="search-result row">
+                <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
+                <div class="col-xs-12 col-sm-12 col-md-2">
+                    <ul class="meta-search">
+                        <li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+                        <li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+                        <li><i class="glyphicon glyphicon-tags"></i> <span>Category1</span></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+                    <h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+                    <span class="tick"><a href="#" title="Item reviewed"><i class="fa fa-check"></i></a></span>
+                    <span class="cross"><a href="#" title="Remove article"><i class="fa fa-times"></i></a></span>
+                </div>
+                <span class="clearfix borda"></span>
+            </article>
+        </section>
+    </div>
+</div>
+
+<?php
+include('template/footer.html');
+?>
