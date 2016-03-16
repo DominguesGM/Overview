8900201e74e623ae52d3ffe60abd6b05ccac018d
Gil Domingues <gmdz95@gmail.com>
2016-03-16 23:21:48 +0000

    Small details changed in the sidebar and index page

--- lbaw_prototype/index.php
+++ lbaw_prototype/index.php
@@ -1,58 +1,50 @@
 <?php
 include('template/header.html');
 ?>
-    <!-- Page Header -->
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">Page Heading
-                <small>Secondary Text</small>
-            </h1>
-        </div>
-    </div>
     <!-- /.row -->
 
     <!-- Projects Row -->
     <div class="row">
         <div class="col-md-4 portfolio-item short_news_item">
-            <h2 class="main_category"><a href="#">Category</a></h2>
+            <h2 class="main_category"><a href="#">Trending</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>First Piece</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Trending Piece</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 portfolio-item short_news_item">
-            <h2 class="main_category"><a href="#">Category</a></h2>
+            <h2 class="main_category"><a href="#">World</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Danger in the Middle East</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Important World News</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 portfolio-item short_news_item">
-            <h2 class="main_category"><a href="#">Category</a></h2>
+            <h2 class="main_category"><a href="#">Europe</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Grexit: is it over yet?</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Will Great Britain stay in Europe</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
@@ -64,45 +56,45 @@ include('template/header.html');
     <!-- Projects Row -->
     <div class="row">
         <div class="col-md-4 portfolio-item short_news_item">
-            <h2 class="main_category"><a href="#">Category</a></h2>
+            <h2 class="main_category"><a href="#">Economy</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>It's all crumbling down</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Take up loans, it's good practice</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 portfolio-item short_news_item">
-            <h2 class="main_category"><a href="#">Category</a></h2>
+            <h2 class="main_category"><a href="#">Sports</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Leichester wins to BarclaysPL</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Local team beats top tier team</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 portfolio-item short_news_item">
-            <h2 class="main_category"><a href="#">Category</a></h2>
+            <h2 class="main_category"><a href="#">Culture</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Best movies so far this year</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>Bands you should listen to</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
@@ -116,12 +108,12 @@ include('template/header.html');
             <h2 class="main_category"><a href="#">Category</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>News Article</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>News Article</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
@@ -131,12 +123,12 @@ include('template/header.html');
             <h2 class="main_category"><a href="#">Category</a></h2>
             <div class="short_category_content">
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>News Article</h3></a>
                     <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
                 <div class="short_category_item">
-                    <a href="#"><h3>News Aricle 1</h3></a>
+                    <a href="#"><h3>News Article</h3></a>
                     <span class="image-box" data-score="123"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et est in tellus fringilla fringilla id eu tellus. Quisque dapibus tristique hendrerit cras amet.</p>
                 </div>
