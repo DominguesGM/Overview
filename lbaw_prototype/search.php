e23745fc13d65a8f33b2756d58bd7baeb1b5ae4f
pmpontes <pedrompontes@outlook.com>
2016-03-16 17:41:44 +0000

    sign in and up

---
+++ lbaw_prototype/search.php
@@ -0,0 +1,83 @@
+<?php
+include('template/header.html');
+?>
+
+<!-- search CSS -->
+<link href="css/search.css" rel="stylesheet">
+
+<?php
+include('template/search_bar.html');
+?>
+<div class="container">
+  <div class="col-md-8 col-xs-offset-2">
+    <hgroup class="mb20">
+      <br>
+      <h1>Search results</h1>
+      <h2 class="lead"><strong class="text-danger">3</strong> results were found for the search for <strong class="text-danger">Lorem
+      </strong></h2>
+    </hgroup>
+  </div>
+
+  <div class="col-md-8 col-xs-offset-2">
+    <section class="col-md-12 col-sm-6 col-md-18">
+      <article class="search-result row">
+        <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
+        <div class="col-xs-12 col-sm-12 col-md-2">
+          <ul class="meta-search">
+            <li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+            <li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+            <li><i class="glyphicon glyphicon-tags"></i> <span>Category1</span></li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+          <h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+          <span class="plus"><a href="#" title="Lorem ipsum"><i class="glyphicon glyphicon-plus"></i></a></span>
+        </div>
+        <span class="clearfix borda"></span>
+      </article>
+
+      <article class="search-result row">
+        <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://placehold.it/75x75" alt=""></span>
+        <div class="col-xs-12 col-sm-12 col-md-2">
+          <ul class="meta-search">
+            <li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+            <li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+            <li><i class="glyphicon glyphicon-tags"></i> <span>Category1</span></li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+          <h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+          <span class="plus"><a href="#" title="Lorem ipsum"><i class="glyphicon glyphicon-plus"></i></a></span>
+        </div>
+        <span class="clearfix borda"></span>
+      </article>
+
+
+      <article class="search-result row">
+        <span class="image-box" data-score="321"><img class="img-thumbnail" src="http://lorempixel.com/250/140/sports" alt=""></span>
+        <div class="col-xs-12 col-sm-12 col-md-2">
+          <ul class="meta-search">
+            <li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+            <li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+            <li><i class="glyphicon glyphicon-tags"></i> <span>Category1</span></li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+          <h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+          <span class="plus"><a href="#" title="Lorem ipsum"><i class="glyphicon glyphicon-plus"></i></a></span>
+        </div>
+        <span class="clearfix borda"></span>
+      </article>
+    </section>
+  </div>
+</div>
+
+<?php
+include('template/footer.html');
+?>
+
+<!-- jQuery -->
+<script src="js/search.js"></script>
--- lbaw_prototype/sign_up.php
+++ lbaw_prototype/sign_up.php
@@ -3,10 +3,67 @@ include('template/header.html');
 ?>
 
 <div class="container">
+
+  <div id="loginbox" style="display:none; margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div class="panel panel-info" >
+      <div class="panel-heading">
+        <div class="panel-title">Sign in</div>
+        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot your password?</a></div>
+      </div>
+
+      <div style="padding-top:30px" class="panel-body" >
+
+        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+
+        <form id="loginform" class="form-horizontal" role="form">
+
+          <div style="margin-bottom: 25px" class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="Email address">
+          </div>
+
+          <div style="margin-bottom: 25px" class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input id="login-password" type="password" class="form-control" name="password" placeholder="Password">
+          </div>
+
+          <div class="input-group">
+            <div class="checkbox">
+              <label>
+                <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
+              </label>
+            </div>
+          </div>
+
+          <div style="margin-top:10px" class="form-group">
+            <!-- Button -->
+            <div class="col-sm-12 controls">
+              <button id="btn-signin" type="button" class="btn btn-primary"><i class="icon-hand-right"></i>Sign in </button>
+              <a id="btn-fblogin" href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Sign in with Facebook </a>
+              <a id="btn-twlogin" href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Sign in with Twitter </a>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-md-12 control">
+              <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                New here?
+                <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
   <div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
     <div class="panel panel-info">
       <div class="panel-heading">
         <div class="panel-title">Sign up</div>
+        <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign in</a></div>
       </div>
       <div class="panel-body" >
         <form id="signupform" class="form-horizontal" role="form">
