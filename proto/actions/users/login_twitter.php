76163fc1358dc70e5d86978a8e3b23a95ccbd85a
pmpontes <pedrompontes@outlook.com>
2016-06-07 02:14:43 +0100

    bug corrected

---
+++ proto/actions/users/login_twitter.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+define('CONSUMER_KEY', getenv('CONSUMER_KEY'));
+define('CONSUMER_SECRET', getenv('CONSUMER_SECRET'));
+define('OAUTH_CALLBACK', getenv('OAUTH_CALLBACK'));
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+
+$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+
+$_SESSION['oauth_token'] = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+
+$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+
+ 
+header('Location:'.$_SERVER['HTTP_REFERER']);
+?>
